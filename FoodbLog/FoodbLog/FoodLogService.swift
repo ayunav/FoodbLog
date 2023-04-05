1a8b84227ec1286c8b061be2f6d9ff66255b45c6
Ikmal Azman <opikmalazman@gmail.com>
2023-04-05 19:13:18 +0100

    create FoodLogService to handle and decode api get request

---
+++ FoodbLog/FoodbLog/FoodLogService.swift
@@ -0,0 +1,41 @@
+//
+//  FoodLogService.swift
+//  FoodbLog
+//
+//  Created by Ikmal Azman on 05/04/2023.
+//  Copyright © 2023 Ayuna Vogel. All rights reserved.
+//
+
+import Foundation
+
+final class FoodLogService {
+    let session = URLSession.shared
+    
+    private var decoder : JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }()
+    
+    func requestAPI<Element : Decodable>(for url : URLRequest, _ type : Element.Type, _ completion : @escaping ((Result<Element,Error>)->Void)) {
+        
+        session.dataTask(with: url) { data, response, error in
+            
+            if let error = error {
+                print("Error requesting API : \(error.localizedDescription)")
+                completion(.failure(error))
+            }
+            
+            guard let data = data else {return}
+            
+            do {
+                let element = try self.decoder.decode(Element.self, from: data)
+                completion(.success(element))
+            } catch {
+                print("Error to decode data : \(error.localizedDescription)")
+                completion(.failure(error))
+            }
+        }
+        .resume()
+    }
+}
