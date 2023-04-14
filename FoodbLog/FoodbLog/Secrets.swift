dfe773ebfe0e2ddd654f41cebfa0e4a9c5307dc5
Ikmal Azman <opikmalazman@gmail.com>
2023-04-14 18:54:38 +0100

    create secrets file

---
+++ FoodbLog/FoodbLog/Secrets.swift
@@ -0,0 +1,11 @@
+//
+//  Secrets.swift
+//  FoodbLog
+//
+//  Created by Ikmal Azman on 14/04/2023.
+//  Copyright © 2023 Ayuna Vogel. All rights reserved.
+//
+
+import Foundation
+
+let API_KEY_UNSPLASH = "FAKE_API_KEY"
